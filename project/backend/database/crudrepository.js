6b55acdbf89f8f1a4ee80d7a96f9a347045aff26
aaltsant <santeri.aaltonen@tuni.fi>
2026-03-11 16:04:20 +0200

    feat(crudrepo, questions.js): more crud operations
    Added deletebyid() and addQuestion() to my app

--- project/backend/database/crudrepository.js
+++ project/backend/database/crudrepository.js
@@ -29,8 +29,38 @@ function findByID(id) {
   });
 }
 
+// Delete location by ID
+function deleteByID(id) {
+  return new Promise((resolve, reject) => {
+    pool.query("DELETE FROM questions WHERE id = ?", [id], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
+}
+
+// Post new question
+function addQuestion(question_text, option_1, option_2, option_3, correct_answer) {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "INSERT INTO questions (question_text, option_1, option_2, option_3, correct_answer) VALUES (?, ?, ?, ?, ?)",
+      [question_text, option_1, option_2, option_3, correct_answer],
+      (error, results) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(results);
+      },
+    );
+  });
+}
+
 module.exports = {
   findAll: findAll,
   findByID: findByID,
+  deleteByID: deleteByID,
+  addQuestion: addQuestion,
   pool: pool,
 };
