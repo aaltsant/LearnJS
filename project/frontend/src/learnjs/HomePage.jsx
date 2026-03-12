0a2d151b083a69ca107d809cca463d2ae1e0d108
aaltsant <santeri.aaltonen@tuni.fi>
2026-03-12 10:10:53 +0200

    feat(HomePage.jsx): Added homepage to frontend

---
+++ project/frontend/src/learnjs/HomePage.jsx
@@ -0,0 +1,13 @@
+function HomePage() {
+
+  return (
+    <div>
+      <h1>JavaScript course project!</h1>
+      <p>In this application you can train your JS knowledge!</p>
+      <p>There is different questions about possible problems and syntax!</p>
+    </div>
+  );
+
+}
+
+export default HomePage
