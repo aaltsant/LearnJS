cf32e97f44ed8fcdf60da3438413187bd5e1c058
aaltsant <santeri.aaltonen@tuni.fi>
2026-03-31 17:23:22 +0300

    feat(AdminTraining.jsx): delete operation
    Delete operation is now fully functional in admin view

--- project/frontend/src/learnjs/admin/AdminTraining.jsx
+++ project/frontend/src/learnjs/admin/AdminTraining.jsx
@@ -1,10 +1,11 @@
-import { use, useState } from 'react';
+import { useState } from 'react';
 import AdminChoosing from './AdminChoosing';
 
 function AdminTraining() {
   const [questions, setQuestions] = useState([]);
   const [view, setView] = useState("Admintraining");
 
+  const [id, setId] = useState("");
   const [question, setQuestion] = useState("");
   const [option1, setOption1] = useState("");
   const [option2, setOption2] = useState("");
@@ -24,24 +25,83 @@ function AdminTraining() {
     return <AdminChoosing />
   }
 
+  if (view === "crudmenu") {
+    return <AdminTraining />
+  }
+
   if (view === "create") {
     return (
       <>
         <form style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
           <h2>Create new question</h2>
-          <input placeholder="question" value={question}/>
-          <input placeholder="option_1" value={option1}/>
-          <input placeholder="option_2" value={option2}/>
-          <input placeholder="option_3" value={option3}/>
-          <input placeholder="correct_answer" value={answer}/>
-          <input placeholder="code _snippet" value={code}/>
-          <input placeholder="feedback_correct" value={correct}/>
-          <input placeholder="feedback_incorrect" value={incorrect}/>
+          <input placeholder="question" value={question} onChange={(e) => setQuestion(e.target.value)} />
+          <input placeholder="option_1" value={option1} onChange={(e) => setOption1(e.target.value)} />
+          <input placeholder="option_2" value={option2} onChange={(e) => setOption2(e.target.value)} />
+          <input placeholder="option_3" value={option3} onChange={(e) => setOption3(e.target.value)} />
+          <input placeholder="correct_answer" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          <input placeholder="code _snippet" value={code} onChange={(e) => setCode(e.target.value)} />
+          <input placeholder="feedback_correct" value={correct} onChange={(e) => setCorrect(e.target.value)} />
+          <input placeholder="feedback_incorrect" value={incorrect} onChange={(e) => setIncorrect(e.target.value)} />
           <button type="submit">Submit</button>
         </form>
-        <button onClick={() => setView("menu")}>Go back</button>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
       </>
     );
+  }
+
+  if (view === "update") {
+    return (
+      <>
+        <form style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
+          <input placeholder='What field you want to update'/>
+          <input placeholder='New value'/>
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
+      </>
+    )
+  }
+
+
+
+  if (view === "delete") {
+    const handleDelete = async (e) => {
+      // tämä estää sivun uudelleen lataamisen (state nollautuisi)
+      e.preventDefault();
+
+      if (!id) {
+        return alert("Give ID!");
+      }
+
+      if (isNaN(id)) {
+        alert("ID is not valid integer!");
+      }
+
+      const response = await fetch(`/api/questions/${id}`, {
+        // tämä kertoo bäkkäriin, että kyseessä on DELETE request
+        method: 'DELETE',
+      });
+
+      // tämä tarkistaa, että onnistuiko poistaminen vai ei
+      // ja ilmoittaa fronttiin siitä. setId() nollaa id:n staten
+      if (response.status === 204) {
+        alert(`Question with ${id} was deleted successfully!`);
+        setId("");
+      } else if (response.status === 404) {
+        alert(`Question with ${id} was not found!`);
+        setId("");
+      }
+    }
+
+    return (
+      <>
+        <form onSubmit={handleDelete} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
+          <input placeholder='Question to delete (ID)' value={id} onChange={(e) => setId(e.target.value)}/>
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
+      </>
+    )
   }
 
   return (
@@ -50,8 +110,8 @@ function AdminTraining() {
       <div>
         <button onClick={() => setView("create")}>Create</button>
         <button onClick={fetchQuestions}>Read</button>
-        <button>Update</button>
-        <button>Delete</button>
+        <button onClick={() => setView("update")}>Update</button>
+        <button onClick={() => setView("delete")}>Delete</button>
       </div>
       <button onClick={() => setView("menu")}>Go back</button>
       <div>
