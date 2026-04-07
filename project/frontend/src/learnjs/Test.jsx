566ccbb4671921291fcd2435977e3e04a78ed6b1
aaltsant <santeri.aaltonen@tuni.fi>
2026-04-07 15:08:57 +0300

    feat(test.jsx): testpage to frontend

--- project/frontend/src/learnjs/Test.jsx
+++ project/frontend/src/learnjs/Test.jsx
@@ -1,9 +1,68 @@
+import { useEffect, useState } from 'react'
+import styles from '../styles/questions.module.css';
+
 function Test() {
+  const [question, setQuestion] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [feedback, setFeedback] = useState("");
+  // const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const response = await fetch("/api/test/rand");
+      const data = await response.json();
+      setQuestion(data);
+    };
+    fetchQuestions();
+  }, []);
+
+  const currentQuestion = question[currentIndex];
+
+  const nextQuestion = () => {
+    setFeedback("");
+    setCurrentIndex(prevIndex => prevIndex + 1);
+  };
+
+  if (!currentQuestion) {
+    return <div>Loading...</div>;
+  }
+
+  const checkAnswer = (answer) => {
+    if (answer === currentQuestion.correct_answer) {
+      setFeedback(currentQuestion.feedback_correct);
+    } else {
+      setFeedback(currentQuestion.feedback_incorrect);
+    }
+  }
+
   return (
-    <>
-      <h1>Testpage</h1>
-    </>
-  )
+    <div>
+      <div>
+        <h2>{currentQuestion.question}</h2>
+          <pre>
+            <code>{currentQuestion.code_snippet}</code>
+          </pre>
+          <div>
+            <button className={styles.button} onClick={() => checkAnswer(currentQuestion.option_1)}>
+              {currentQuestion.option_1}
+            </button>
+
+            <button className={styles.button} onClick={() => checkAnswer(currentQuestion.option_2)}>
+              {currentQuestion.option_2}
+            </button>
+
+            <button className={styles.button} onClick={() => checkAnswer(currentQuestion.option_3)}>
+              {currentQuestion.option_3}
+            </button>
+
+            <p>{feedback}</p>
+          </div>
+          <button className={styles.nextButton} onClick={nextQuestion}>
+            Next question
+          </button>
+      </div>
+    </div>
+  );
 }
 
 export default Test
--- project/frontend/src/learnjs/admin/AdminTest.jsx
+++ project/frontend/src/learnjs/admin/AdminTest.jsx
@@ -2,31 +2,203 @@ import { useState } from 'react';
 import AdminChoosing from './AdminChoosing';
 
 function AdminTest() {
-  const [question, setQuestion] = useState([]);
+  const [questions, setQuestions] = useState([]);
   const [view, setView] = useState("Admintest");
 
+  const [id, setId] = useState("");
+  const [field, setField] = useState("");
+  const [newField, setNewField] = useState("");
+
+  const [question, setQuestion] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [code, setCode] = useState();
+  const [correct, setCorrect] = useState("");
+  const [incorrect, setIncorrect] = useState("");
+
   const fetchQuestions = async () => {
-    const response = await fetch("/api/testquestions");
+    const response = await fetch("/api/test");
     const data = await response.json();
-    setQuestion(data);
+    setQuestions(data);
   };
 
   if (view === "menu") {
     return <AdminChoosing />
   }
 
+  if (view === "crudmenu") {
+    return <AdminTest />
+  }
+
+  if (view === "create") {
+    const handleCreate = async (e) => {
+      e.preventDefault();
+
+      const response = await fetch(`api/test`, {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+          question: question,
+          option_1: option1,
+          option_2: option2,
+          option_3: option3,
+          correct_answer: answer,
+          code_snippet: code,
+          feedback_correct: correct,
+          feedback_incorrect: incorrect
+        })
+      });
+
+      if (response.status === 201) {
+        alert(`New question created successfully`);
+        setQuestion("");
+        setOption1("");
+        setOption2("");
+        setOption3("");
+        setAnswer("");
+        setCode();
+        setCorrect("");
+        setIncorrect("");
+      } else {
+        alert(`Something went wrong!`);
+      }
+    }
+
+    return (
+      <>
+        <form onSubmit={handleCreate} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px', padding: '15px' }}>
+          <h2>Create new question</h2>
+          <input placeholder="question" value={question} onChange={(e) => setQuestion(e.target.value)} />
+          <input placeholder="option_1" value={option1} onChange={(e) => setOption1(e.target.value)} />
+          <input placeholder="option_2" value={option2} onChange={(e) => setOption2(e.target.value)} />
+          <input placeholder="option_3" value={option3} onChange={(e) => setOption3(e.target.value)} />
+          <input placeholder="correct_answer" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          <input placeholder="code_snippet" value={code} onChange={(e) => setCode(e.target.value)} />
+          <input placeholder="feedback_correct" value={correct} onChange={(e) => setCorrect(e.target.value)} />
+          <input placeholder="feedback_incorrect" value={incorrect} onChange={(e) => setIncorrect(e.target.value)} />
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
+      </>
+    );
+  }
+
+  if (view === "update") {
+    const handleUpdate = async (e) => {
+      e.preventDefault();
+
+      const correctFields = ["question", "option_1", "option_2", "option_3",
+      "correct_answer", "code_snippet", "feedback_correct", "feedback_incorrect"]
+
+      if (!id) {
+        return alert("Give ID!");
+      }
+
+      if (isNaN(id)) {
+        alert("ID is not valid integer!");
+      }
+
+      if (!correctFields.includes(field)) {
+        alert("Field you want to update dont exist!");
+        return;
+      }
+
+      if (!newField) {
+        return alert("Give new value!");
+      }
+
+      const response = await fetch(`api/test/${id}`, {
+        // kertoo mikä metodi
+        method: 'PATCH',
+        // tämä kertoo, että tuleva sisältö on jsonia
+        headers: { 'Content-type': 'application/json' },
+        // Tämä muuttaa js-olion merkkijonoksi
+        // se täytyy muuttaa merkkijonoksi, jotta bäkkäri osaa lukea sitä!
+        body: JSON.stringify({
+          [field]: newField
+        })
+      });
+
+      if (response.status === 200) {
+        alert(`Question with id: ${id} was updated successfully!`);
+        setId("");
+        setField("");
+        setNewField("");
+      } else if (response.status === 404) {
+        alert(`Question with id: ${id} was not found!`);
+      } else {
+        alert("Something went wrong!");
+      }
+    }
+
+    return (
+      <>
+        <form onSubmit={handleUpdate} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px', padding: '15px'}}>
+          <input placeholder='Questions ID' value={id} onChange={(e) => setId(e.target.value)}/>
+          <input placeholder='Field you want to update' value={field} onChange={(e) => setField(e.target.value)}/>
+          <input placeholder='New value' value={newField} onChange={(e) => setNewField(e.target.value)}/>
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
+      </>
+    )
+  }
+
+
+
+  if (view === "delete") {
+    const handleDelete = async (e) => {
+      // tämä estää sivun uudelleen lataamisen (state nollautuisi)
+      e.preventDefault();
+
+      if (!id) {
+        return alert("Give ID!");
+      }
+
+      if (isNaN(id)) {
+        alert("ID is not valid integer!");
+      }
+
+      const response = await fetch(`/api/test/${id}`, {
+        // tämä kertoo bäkkäriin, että kyseessä on DELETE request
+        method: 'DELETE',
+      });
+
+      // tämä tarkistaa, että onnistuiko poistaminen vai ei
+      // ja ilmoittaa fronttiin siitä. setId() nollaa id:n staten
+      if (response.status === 204) {
+        alert(`Question with id: ${id} was deleted successfully!`);
+        setId("");
+      } else if (response.status === 404) {
+        alert(`Question with id: ${id} was not found!`);
+      }
+    }
+
+    return (
+      <>
+        <form onSubmit={handleDelete} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px', padding: '15px' }}>
+          <input placeholder='Question to delete (ID)' value={id} onChange={(e) => setId(e.target.value)}/>
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={() => setView("crudmenu")}>Go back</button>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>Test table CRUD -operations</h1>
       <div>
-        <button>Create</button>
+        <button onClick={() => setView("create")}>Create</button>
         <button onClick={fetchQuestions}>Read</button>
-        <button>Update</button>
-        <button>Delete</button>
+        <button onClick={() => setView("update")}>Update</button>
+        <button onClick={() => setView("delete")}>Delete</button>
       </div>
       <button onClick={() => setView("menu")}>Go back</button>
       <div>
-        <pre>{JSON.stringify(question, null, 2)}</pre>
+        <pre>{JSON.stringify(questions, null, 2)}</pre>
       </div>
     </>
   );
