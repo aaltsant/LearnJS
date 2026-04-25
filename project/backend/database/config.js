13d166c5c26a3188d802fe7104c24fbcf4d9e364
aaltsant <santeri.aaltonen@tuni.fi>
2026-04-25 21:19:24 +0300

    fix(config.js)

---
+++ project/backend/database/config.js
@@ -0,0 +1,10 @@
+let config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  ssl: { rejectUnauthorized: false },
+};
+
+module.exports = config;
