e997ea92f95d5c21170e244793c9c768a0a5ca80
aaltsant <santeri.aaltonen@tuni.fi>
2026-04-25 18:10:23 +0300

    fix(HomePage.jsx): home text changes to mode cards

--- project/frontend/src/learnjs/HomePage.jsx
+++ project/frontend/src/learnjs/HomePage.jsx
@@ -22,25 +22,33 @@ function HomePage() {
   return (
     <div>
       <h1 className={styles.header}>LearnJS! - JavaScript made fun!</h1>
-      <h2 className={styles.header2}>About this project:</h2>
-      <p className={styles.para}>
-        LearnJS! is my first ever fullstack project. I had an idea about making this project for a long time.
-        I made this as my fullstack course project because I wanted to make project that will taught me new things.
-        Whether you are beginner or an advanced coder you can learn new things from here, try it out to sharpen your skills and get your name up on the leaderboard!
-      </p>
+      <div className={styles.about}>
+        <h2 className={styles.header2}>About this project:</h2>
+        <p className={styles.para}>
+          LearnJS! is my first ever fullstack project. I had an idea about making this project for a long time.
+          I made this as my fullstack course project because I wanted to make project that will taught me new things.
+          Whether you are beginner or an advanced coder you can learn new things from here, try it out to sharpen your skills and get your name up on the leaderboard!
+        </p>
+      </div>
 
       <div className={styles.maincontainer}>
 
         <div className={styles.modecard}>
           <h3 className={styles.header3}>Training mode</h3>
-          <p className={styles.modepara}>Here you can train your knowledge and skills. There is theory questions and syntax questions!</p>
-          <p className={styles.modepara}>Master the basics at your own pace. Practice with a vast library of questions ranging from variables to asynchronous patterns. Perfect for learning without the pressure of a timer.</p>
+          <p className={styles.modepara}>
+            Master the basics at your own pace. Practice with a vast library of questions ranging from variables to asynchronous patterns.
+            Build your confidence and discover the correct answers without losing your streak.
+            Perfect for learning without the pressure of a timer.
+            </p>
         </div>
 
         <div className={styles.modecard}>
           <h3 className={styles.header3}>Test mode</h3>
-          <p className={styles.modepara}>Here you can really  put your skills to test! Saves your username and points.</p>
-          <p className={styles.modepara}>Put your JS knowledge to the ultimate test! Answer 15 randomized questions. Your performance will be ranked, so stay sharp and aim for the top of the leaderboard.</p>
+          <p className={styles.modepara}>
+            Put your JS knowledge to the ultimate test! Answer 15 randomized questions.
+            Build your streak by answering correctly in a row to multiply your score!
+            Your performance will be ranked, so stay sharp and aim for the top of the leaderboard.
+          </p>
         </div>
 
         <div className={styles.leaderboard}>
